7c414e4fd58cc4dfac0a7384209666838bfe2f54
Quang Akiyama <quanganhcf2@gmail.com>
2024-09-17 20:08:43 +0700

    fix bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense, useState } from "react";
 import Loader from "../components/Loader";
-import Island from "../models/island";
+import Island from "../models/Island";
 import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
